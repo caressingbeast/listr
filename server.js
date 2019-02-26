b0c9290f23cadb2837c2407dd593ea8fce3d7248
Brandon Burning <brandonb@ecreations.net>
2019-02-25 23:56:32 -0700

    Getting it working on Heroku

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const express = require('express');
 const helmet = require('helmet');
 const mongoose = require('mongoose');
+const path = require('path');
 
 require('dotenv').config();
 
@@ -25,6 +26,14 @@ mongoose.connect(mongoUrl, { useCreateIndex: true, useNewUrlParser: true }, (err
 });
 
 require('./routes.js')(app);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+}
+
+app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 let port = process.env.PORT || 5000;
 app.listen(port, () => {
